8dcad1d2b44e483629823b2505fd9698dd904a7e
Casey Grzecka <cgrzecka@library.ucla.edu>
2017-06-01 16:20:13 -0700

    resolves #2 by adding page as the base hook. also limits the hijacking to the statsform path as arg(0)

--- page--hijacked.tpl.php
+++ page--hijacked.tpl.php
@@ -1,3 +1,4 @@
 <h1>This page is hijacked</h1>
 <div>Notice that the regions of the body are gone and replaced by this template. You will have to learn how to use hook_html_head_alter() in order to modify things in the head element, like jquery version loaded, etc.</div>
+<?php print $messages; ?>
 <div><?php print render($page['content']); ?></div>
